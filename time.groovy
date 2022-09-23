2b5dcf095dd79dfe1a516f63952c57ff2844cbc2
Anchekrishna <96217535+Anchekrishna@users.noreply.github.com>
2022-09-22 20:20:40 -0400

    Add files via upload

---
+++ time.groovy
@@ -0,0 +1,28 @@
+import com.invest.utils.ParamBinder
+
+def call(body) {
+    Map params = [
+            APP_NAME: null,
+            PACKAGING: null,
+            AWS_S3_BUCKET_NAME  : null,
+            S3_ARTIFACT_PATH    : null
+    ]
+
+    ParamBinder.bind(params, this, body)
+
+    pipeline {
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                sh '''date > test
+                cat test'''
+            }
+        }
+    }    
+    post {
+        always 
+            {archiveArtifacts artifacts: 'test', followSymlinks: false}
+        }
+}
