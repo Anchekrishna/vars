8ca303b81cc8cf672cb0f1f71b1f376445b889a5
Anchekrishna <96217535+Anchekrishna@users.noreply.github.com>
2022-09-26 17:08:08 -0400

    Update time.groovy

--- timee/time.groovy
+++ timee/time.groovy
@@ -1,12 +1,4 @@
-
-
 def call(body) {
-    Map params = [
-            APP_NAME: null,
-            PACKAGING: null,
-            AWS_S3_BUCKET_NAME  : null,
-            S3_ARTIFACT_PATH    : null
-    ]
 
     ParamBinder.bind(params, this, body)
 
