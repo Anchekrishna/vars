624b17996f9e120b2d9e289cdf3e537aa30d737d
Anchekrishna <96217535+Anchekrishna@users.noreply.github.com>
2022-09-26 17:19:12 -0400

    Update timee.groovy

--- vars/timee.groovy
+++ vars/timee.groovy
@@ -1,1 +1,18 @@
+def call(body) {
+    pipeline {
+    agent any
 
+    stages {
+        stage('Hello') {
+            steps {
+                sh '''date > test
+                cat test'''
+            }
+        }
+    }    
+    post {
+        always 
+            {archiveArtifacts artifacts: 'test', followSymlinks: false}
+        }
+}
+}
